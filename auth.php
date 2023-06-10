baa51d9b853cbada4e927ea1b4dcc9f9137adfd1
jasinramadani04 <76982137+jasinramadani04@users.noreply.github.com>
2023-06-10 11:32:35 +0200

    Create auth.php

---
+++ auth.php
@@ -0,0 +1,9 @@
+<?php
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+    // Në vend të këtij rreshti, mund të vendosni logjikën tuaj për shkëmbimin e të dhënave me Termux.
+    // Për qëllime demonstruese, kemi vetëm shfaqur të dhënat e shkruara në formular.
+    echo "Emri i përdoruesit: $username<br>";
+    echo "Fjalëkalimi: $password<br>";
+?>
